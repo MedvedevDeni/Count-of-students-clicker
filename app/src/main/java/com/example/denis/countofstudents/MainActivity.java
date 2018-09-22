a649d017caa0a4ecbb1115d7cd688db323b8482d
Denis <dan11.09@bk.ru>
2018-09-22 14:34:47 +0300

    Жизненный цикл Activity добавлен

--- app/src/main/java/com/example/denis/countofstudents/MainActivity.java
+++ app/src/main/java/com/example/denis/countofstudents/MainActivity.java
@@ -2,12 +2,85 @@ package com.example.denis.countofstudents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter", counter);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.containsKey("counter")) {
+            counter = savedInstanceState.getInt("counter");
+        }
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        resetUI();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        resetUI();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    private void resetUI() {
+        ((TextView) findViewById(R.id.numberOfStudents)).setText(counter.toString());
+        Log.d(TAG, "resetUI");
+    }
+
+    public void onClickBtnStudents(View view) {
+        counter++;
+        TextView counterViev = (TextView) findViewById(R.id.numberOfStudents);
+        counterViev.setText(counter.toString());
     }
 }
